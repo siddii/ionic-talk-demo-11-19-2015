903aaffe56e575190570d6b82bf730ea5d07eb81
Siddique Hameed <siddique_hameed@mastercard.com>
2015-11-19 14:10:04 -0600

    First Working revision

---
+++ STLJSIonicApp/www/js/controllers.js
@@ -0,0 +1,57 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+}).controller('ProfileCtrl', function($scope, $cordovaCamera) {
+        $scope.name = "Stranger Dude";
+
+        document.addEventListener("deviceready", function () {
+
+            var options = {
+                quality: 50,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                targetWidth: 300,
+                targetHeight: 300,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: false,
+                correctOrientation:true
+            };
+
+            $scope.takeProfilePicture = function (){
+                $cordovaCamera.getPicture(options).then(function(imageData) {
+                    var image = document.getElementById('profilePicture');
+                    image.src = "data:image/jpeg;base64," + imageData;
+                }, function(err) {
+                    alert(err);
+                });
+            };
+
+        }, false);
+
+});
